33606b71d5f42049605835a53a0e4a558adb012b
LongLe <lelinhlong@gmail.com>
2025-07-08 20:21:42 +0700

    again

--- add_source.py
+++ add_source.py
@@ -6,6 +6,6 @@ lv3 = pd.read_csv('3.csv')
 lv1['Source'] = 'Level 1'
 lv2['Source'] = 'Level 2'
 lv3['Source'] = 'Level 3'
-wildcard['Source'] = 'wildcard'
+wildcard['Source'] = 'Wildcard'
 lvwc = pd.concat([lv1, lv2, lv3, wildcard], ignore_index=True)
 lvwc.to_csv('Full.csv', index=False)
